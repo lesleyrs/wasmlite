1fbba67f947e67daa0839d392072429f76909313
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2025-08-11 02:02:27 +0200

    add minecraft clone assets

---
+++ ports/res/shaders/basic2d.fs
@@ -0,0 +1,14 @@
+#version 300 es
+precision mediump float;
+
+uniform sampler2D tex;
+uniform bool use_tex;
+
+in vec4 v_color;
+in vec2 v_uv;
+
+out vec4 frag_color;
+
+void main() {
+    frag_color = (use_tex ? texture(tex, v_uv) : vec4(1.0)) * v_color;
+}
