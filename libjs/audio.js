60766bd3dc93b2ea23aefdd635e59dda6ee11234
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2025-08-18 03:42:23 +0200

    improve audio streaming

--- libjs/audio.js
+++ libjs/audio.js
@@ -1,12 +1,19 @@
 import { refreshMemory, exports, u8, f32 } from './loader.js'
 
 // TODO doom wants panning, stopAudio, isPlaying, doom sound explosion caused by those missing calls?
-// NOTE we can't call wasm functions in AudioWorklet without SharedArrayBuffer + --import-memory, so unused
+
+// we can't avoid choppy audio with ScriptProcessorNode whenever main thread is busy
+// we can't call wasm functions in AudioWorklet without SharedArrayBuffer + --import-memory + more changes
+// instead of callbacks you have to push the buffers yourself by calling JS_streamAudio/JS_startAudio
 
 const audioCtx = new AudioContext();
 // TODO streamGain
 let audioGain = audioCtx.createGain();
 audioGain.connect(audioCtx.destination);
+
+let streamTime;
+const streamLatency = 0.1;
+const streamLength = 1;
 
 export const audio = {
   JS_startAudio: async (buf, len) => {
@@ -15,10 +22,10 @@ export const audio = {
     // get arraybuffer slice instead of subarray view or memory will get detached
     const arrayBuffer = audio.buffer.slice(audio.byteOffset, audio.byteOffset + audio.byteLength);
     const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
-    let bufferSource = audioCtx.createBufferSource();
-    bufferSource.buffer = audioBuffer;
-    bufferSource.connect(audioGain);
-    bufferSource.start();
+    let source = audioCtx.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(audioGain);
+    source.start();
   },
   JS_setAudioVolume: (vol) => {
     audioGain.gain.value = vol;
@@ -26,15 +33,47 @@ export const audio = {
   JS_getSampleRate: () => {
     return audioCtx.sampleRate;
   },
-  JS_streamAudio: (cb, userdata, stream, samples) => {
+  JS_resumeAudio: () => {
     document.addEventListener("click", () => audioCtx.resume());
+  },
+  JS_streamAudio: (cb, userdata, stream, samples) => {
+    const now = audioCtx.currentTime;
+    streamTime = streamTime || now + streamLatency; // add initial now+latency offset
+
+    if (streamTime < now + streamLength) {
+      refreshMemory();
+
+      const channels = 2;
+      const frames = samples / (channels * Float32Array.BYTES_PER_ELEMENT);
+      let buffer = audioCtx.createBuffer(channels, frames, audioCtx.sampleRate);
+
+      exports.__indirect_function_table.get(cb)(userdata, stream, samples);
+
+      const outL = buffer.getChannelData(0);
+      const outR = buffer.getChannelData(1);
+      for (let i = 0; i < frames; i++) {
+        const idx = (stream >> 2) + i * 2;
+        outL[i] = f32[idx];
+        outR[i] = f32[idx + 1];
+      }
+
+      let source = audioCtx.createBufferSource();
+      source.buffer = buffer;
+      source.connect(audioCtx.destination);
+      source.start(streamTime);
+
+      streamTime += buffer.duration;
+    }
+  },
+  // NOTE: must only be called once
+  /* JS_streamAudio: (cb, userdata, stream, samples) => {
+    refreshMemory();
 
     const channels = 2;
     const frames = samples / (channels * Float32Array.BYTES_PER_ELEMENT);
     const processor = audioCtx.createScriptProcessor(frames, 0, channels);
 
     processor.onaudioprocess = (e) => {
-      refreshMemory();
       exports.__indirect_function_table.get(cb)(userdata, stream, samples);
 
       const outL = e.outputBuffer.getChannelData(0);
@@ -47,5 +86,5 @@ export const audio = {
     };
 
     processor.connect(audioCtx.destination);
-  },
+  }, */
 }
