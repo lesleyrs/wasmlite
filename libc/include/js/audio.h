6ceb2295061902de69df07404454f98bf51d8453
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2025-08-20 17:20:59 +0200

    use audioworklets for queueAudio + setAudioCallback

--- libc/include/js/audio.h
+++ libc/include/js/audio.h
@@ -3,10 +3,14 @@
 
 #include "wasm.h"
 
-typedef void (*JS_AudioCallback)(void *userdata, uint8_t *stream, int samples);
-
 WASM_IMPORT(void, JS_startAudio, (uint8_t *buf, int len));
 WASM_IMPORT(void, JS_setAudioVolume, (double vol));
+
 WASM_IMPORT(double, JS_getSampleRate, (void));
-WASM_IMPORT(void, JS_resumeAudio, (void));
-WASM_IMPORT(void, JS_streamAudio, (JS_AudioCallback, void *userdata, uint8_t *stream, int samples));
+WASM_IMPORT(void, JS_resumeAudio, (double freq));
+
+WASM_IMPORT(int, JS_getQueuedAudioSize, (void));
+WASM_IMPORT(void, JS_queueAudio, (void *stream, int bytes));
+
+typedef void (*JS_AudioCallback)(void *userdata, uint8_t *stream, int samples);
+WASM_IMPORT(void, JS_setAudioCallback, (JS_AudioCallback, void *userdata, void *stream, int bytes));
--- libjs/audio.js
+++ libjs/audio.js
@@ -1,25 +1,54 @@
 import { refreshMemory, exports, u8, f32 } from './loader.js'
 
-// TODO doom wants panning, stopAudio, isPlaying, doom sound explosion caused by those missing calls?
-
-// we can't avoid choppy audio with ScriptProcessorNode whenever main thread is busy
-// we can't call wasm functions in AudioWorklet without SharedArrayBuffer + --import-memory + more changes
-// instead of callbacks you have to push the buffers yourself by calling JS_streamAudio/JS_startAudio
+const workletSource = `
+class Processor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.queue = [];
+    this.pos = 0;
+    this.current = null;
+    this.requested = false;
+    this.port.onmessage = e => {
+      if (e.data.pcm) {this.queue.push(e.data.pcm); this.requested = false; };
+      if (e.data.queuedAudioSize) this.port.postMessage({queuedAudioSize: this.queue.reduce((sum, buf) => sum + buf.length * 4, 0)});
+    };
+  }
+  process(_, outputs) {
+    const [outL, outR] = outputs[0];
+    if (!this.current || this.pos >= this.current.length) {
+      this.current = this.queue.shift() || null;
+      this.pos = 0;
+      if (this.queue.length <= 1 && !this.requested) { this.port.postMessage({request: true}); this.requested = true; }
+    }
+    for (let i = 0; i < outL.length; i++) {
+      if (this.current) {
+        outL[i] = this.current[this.pos++];
+        outR[i] = this.current[this.pos++];
+      }
+    }
+    return true;
+  }
+}
+registerProcessor("processor", Processor);
+`;
 
 const audioCtx = new AudioContext();
-// TODO streamGain
 let audioGain = audioCtx.createGain();
 audioGain.connect(audioCtx.destination);
 
-let streamTime;
-const streamLatency = 0.1;
-const streamLength = 1;
+// TODO streamGain?
+let streamCtx;
+// let streamTime;
+// const streamLatency = 0.1;
+// const streamLength = 1;
+
+let worklet;
 
 export const audio = {
   JS_startAudio: async (buf, len) => {
     refreshMemory();
     const audio = u8.subarray(buf, buf + len);
-    // get arraybuffer slice instead of subarray view or memory will get detached
+    // slice instead of subarray view or the memory be detached
     const arrayBuffer = audio.buffer.slice(audio.byteOffset, audio.byteOffset + audio.byteLength);
     const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
     let source = audioCtx.createBufferSource();
@@ -31,23 +60,74 @@ export const audio = {
     audioGain.gain.value = vol;
   },
   JS_getSampleRate: () => {
-    return audioCtx.sampleRate;
+    return streamCtx.sampleRate;
   },
-  JS_resumeAudio: () => {
-    document.addEventListener("click", () => audioCtx.resume());
+  JS_resumeAudio: new WebAssembly.Suspending(async (sampleRate) => {
+    // TODO move some code out?
+    streamCtx = new AudioContext({ sampleRate: sampleRate });
+    document.addEventListener("click", () => streamCtx.resume());
+
+    const blob = new Blob([workletSource], { type: "application/javascript" });
+    const url = URL.createObjectURL(blob);
+    await streamCtx.audioWorklet.addModule(url);
+
+    worklet = new AudioWorkletNode(streamCtx, 'processor', {
+      outputChannelCount: [2],
+    });
+
+    worklet.connect(streamCtx.destination);
+  }),
+  JS_setAudioCallback: (cb, userdata, stream, bytes) => {
+    worklet.port.onmessage = (e) => {
+      if (e.data.request) {
+        refreshMemory();
+        if (cb) {
+          exports.__indirect_function_table.get(cb)(userdata, stream, bytes);
+        }
+        const stream32 = stream >> 2, samples32 = bytes >> 2;
+        const tmp = new Float32Array(f32.subarray(stream32, stream32 + samples32));
+        worklet.port.postMessage({pcm: tmp}, [tmp.buffer]);
+      }
+    };
   },
-  JS_streamAudio: (cb, userdata, stream, samples) => {
-    const now = audioCtx.currentTime;
+  JS_queueAudio: (stream, bytes) => {
+    refreshMemory();
+    const stream32 = stream >> 2, samples32 = bytes >> 2;
+    const tmp = new Float32Array(f32.subarray(stream32, stream32 + samples32));
+    worklet.port.postMessage({pcm: tmp}, [tmp.buffer]);
+  },
+  JS_getQueuedAudioSize: new WebAssembly.Suspending(async () => {
+    // TODO: avoid recreating onmessage?
+    const size = await new Promise(resolve => {
+      worklet.port.onmessage = (e) => {
+        if ('queuedAudioSize' in e.data) {
+          resolve(e.data.queuedAudioSize);
+        }
+      }
+      worklet.port.postMessage({queuedAudioSize: true});
+    })
+
+    return size;
+  }),
+  /* JS_queueAudio: (cb, userdata, stream, samples) => {
+    const now = streamCtx.currentTime;
     streamTime = streamTime || now + streamLatency; // add initial now+latency offset
+
+    if (streamTime < now) {
+      console.log('Syncing audio... ' + streamTime.toFixed(2) + ' / ' + now.toFixed(2));
+      streamTime = now + streamLatency;
+    }
 
     if (streamTime < now + streamLength) {
       refreshMemory();
 
       const channels = 2;
       const frames = samples / (channels * Float32Array.BYTES_PER_ELEMENT);
-      let buffer = audioCtx.createBuffer(channels, frames, audioCtx.sampleRate);
+      let buffer = streamCtx.createBuffer(channels, frames, streamCtx.sampleRate);
 
-      exports.__indirect_function_table.get(cb)(userdata, stream, samples);
+      if (cb) {
+        exports.__indirect_function_table.get(cb)(userdata, stream, samples);
+      }
 
       const outL = buffer.getChannelData(0);
       const outR = buffer.getChannelData(1);
@@ -57,24 +137,25 @@ export const audio = {
         outR[i] = f32[idx + 1];
       }
 
-      let source = audioCtx.createBufferSource();
+      let source = streamCtx.createBufferSource();
       source.buffer = buffer;
-      source.connect(audioCtx.destination);
+      source.connect(streamCtx.destination);
       source.start(streamTime);
 
       streamTime += buffer.duration;
     }
-  },
-  // NOTE: must only be called once
-  /* JS_streamAudio: (cb, userdata, stream, samples) => {
+  }, */
+  /* JS_setAudioCallback: (cb, userdata, stream, samples) => {
     refreshMemory();
 
     const channels = 2;
     const frames = samples / (channels * Float32Array.BYTES_PER_ELEMENT);
-    const processor = audioCtx.createScriptProcessor(frames, 0, channels);
+    const processor = streamCtx.createScriptProcessor(frames, 0, channels);
 
     processor.onaudioprocess = (e) => {
-      exports.__indirect_function_table.get(cb)(userdata, stream, samples);
+      if (cb) {
+        exports.__indirect_function_table.get(cb)(userdata, stream, samples);
+      }
 
       const outL = e.outputBuffer.getChannelData(0);
       const outR = e.outputBuffer.getChannelData(1);
@@ -85,6 +166,6 @@ export const audio = {
       }
     };
 
-    processor.connect(audioCtx.destination);
+    processor.connect(streamCtx.destination);
   }, */
 }
