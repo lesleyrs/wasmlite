55a64795a19ed664c18e42e54f408eba89b8337a
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2026-03-13 13:47:27 +0100

    add optional len to parseInt/Float without needing null term

--- libc/include/js/glue.h
+++ libc/include/js/glue.h
@@ -7,8 +7,8 @@
 
 // UTIL:
 WASM_IMPORT(void, JS_logFloat, (double x));
-WASM_IMPORT(int, JS_parseInt, (const char* x));
-WASM_IMPORT(double, JS_parseFloat, (const char* x));
+WASM_IMPORT(int, JS_parseInt, (const char* x, size_t len));
+WASM_IMPORT(double, JS_parseFloat, (const char* x, size_t len));
 WASM_IMPORT(void, JS_eval, (const char* str));
 WASM_IMPORT(void, JS_alert, (const char* msg));
 WASM_IMPORT(void, JS_saveFile, (const char* name, uint8_t *buf, int len));
